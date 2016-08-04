07d261b1e317750e35b9adaccd25882b506906f5
Bela <bela.stoyan@gmail.com>
2016-08-05 01:50:52 +0200

    URL Safe Tokens

--- oauth_login.js
+++ oauth_login.js
@@ -45,7 +45,7 @@ setTimeout(function() {
 		function randomCharacters(length)
 		{
 		    var text = "";
-		    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789/*-.,";
+		    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
 
 		    for( var i=0; i < length; i++ )
 		        text += possible.charAt(Math.floor(Math.random() * possible.length));
