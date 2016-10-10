e2c257f0488601dd6893f2706a60e81bce0094f3
kirschn <github@kirschn.de>
2016-10-11 00:43:11 +0200

    aaaand allow-headers

--- api.js
+++ api.js
@@ -61,13 +61,13 @@ if (cluster.isMaster) {
           app.use(function (req, res, next) {
 
               // Website you wish to allow to connect
-              res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+              res.setHeader('Access-Control-Allow-Origin', '*');
 
               // Request methods you wish to allow
               res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
               // Request headers you wish to allow
-              res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+              res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
 
               // Set to true if you need the website to include cookies in the requests sent
               // to the API (e.g. in case you use sessions)
