2814ac4e4d179ba79a9920d960c4708bf58c946a
kirschn <github@kirschn.de>
2016-10-11 00:38:34 +0200

    addded CORS handling

--- api.js
+++ api.js
@@ -57,6 +57,25 @@ if (cluster.isMaster) {
           var app = express();
           app.use(bodyParser.json()); // for parsing application/json
           app.use(bodyParser.urlencoded({ extended: true }));
+          // Add headers
+          app.use(function (req, res, next) {
+
+              // Website you wish to allow to connect
+              res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+              // Request methods you wish to allow
+              res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+              // Request headers you wish to allow
+              res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+              // Set to true if you need the website to include cookies in the requests sent
+              // to the API (e.g. in case you use sessions)
+              res.setHeader('Access-Control-Allow-Credentials', true);
+
+              // Pass to next layer of middleware
+              next();
+          });
           app.put('/v1/follow/:user', function (req, res) {
               if (req.get("Authorization") !== undefined && req.params.user !== undefined) {
                 if (parseInt(req.params.user) !== NaN) {
